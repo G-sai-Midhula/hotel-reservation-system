7b522bfa6388edc75280cfd43f60e5635f44dcb4
G_Sai_Midhula <midhuroy17@gmail.com>
2026-07-02 10:37:02 +0530

    Remove hardcoded data, add Procfile, ready for Railway

--- src/main/java/com/hotel/reservation/ReservationApplication.java
+++ src/main/java/com/hotel/reservation/ReservationApplication.java
@@ -2,8 +2,6 @@ package com.hotel.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReservationApplication {
@@ -12,26 +10,4 @@ public class ReservationApplication {
         SpringApplication.run(ReservationApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner loadData(RoomRepository roomRepository) {
-        return args -> {
-            Room r1 = new Room();
-            r1.setRoomType("Single");
-            r1.setPrice(1500);
-            r1.setAvailable(true);
-            roomRepository.save(r1);
-
-            Room r2 = new Room();
-            r2.setRoomType("Double");
-            r2.setPrice(2500);
-            r2.setAvailable(true);
-            roomRepository.save(r2);
-
-            Room r3 = new Room();
-            r3.setRoomType("Deluxe");
-            r3.setPrice(4000);
-            r3.setAvailable(false);
-            roomRepository.save(r3);
-        };
-    }
 }
